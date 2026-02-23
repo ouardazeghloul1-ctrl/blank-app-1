5228260bd9f6f737418bf2dca0fd7bb304456b59
ouardazeghloul1-ctrl <ouardazeghloul1@gmail.com>
2026-02-23 01:51:39 +0100

    Delete robo_router.py

--- robo_router.py
+++
@@ -1,7 +0,0 @@
-# robo_chat/robo_router.py
-
-from robo_chat.robo_brain import RoboAdvisor
-
-def handle_robo_question(user_profile, alerts_today, question):
-    robo = RoboAdvisor(user_profile, alerts_today)
-    return robo.answer(question)
