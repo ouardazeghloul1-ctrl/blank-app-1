5055ee658b838c48c2fadc80b695bb6f597accfb
ouardazeghloul1-ctrl <ouardazeghloul1@gmail.com>
2026-03-01 01:29:03 +0100

    Update market_data_core.py

--- market_data_core.py
+++ market_data_core.py
@@ -1,16 +1,11 @@
 from government_data_provider import load_government_data
 
+
 def get_market_data(city=None, property_type=None):
 
     df = load_government_data(selected_city=city)
 
     if df is None or df.empty:
         raise Exception(f"❌ لا توجد بيانات متاحة لـ {city}")
-
-    df = df.rename(columns={
-        "السعر": "price",
-        "المساحة": "area",
-        "الحي": "district"
-    })
 
     return df.reset_index(drop=True)
